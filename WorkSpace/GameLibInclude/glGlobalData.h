29bdaaa352c659580751e8779875aa2f51f3ee87
NekoMasutaDev <NekoMasutaDev@outlook.com>
2019-09-23 00:07:55 +0800

    .

--- WorkSpace/GameLibInclude/glGlobalData.h
+++ WorkSpace/GameLibInclude/glGlobalData.h
@@ -1,21 +1,8 @@
 
 #pragma once
 
-#include "glWindow.h"
-#include "glVideoDevice.h"
-#include "glSoundDevice.h"
-#include "glBatchSprite.h"
-#include "glBatchPrimitive.h"
 #include "glModule.h"
 
-extern glWindow gWindow;
-extern glVideoDevice gVideoDevice;
-extern glSoundDevice gSoundDevice;
-extern glBatchSprite gBatchSprite;
-extern glBatchPrimitive gBatchPrimitive;
-extern glModule gModule_D3D9;
-extern glModule gModule_D3DX9;
-extern glModule gModule_DSound;
 extern glModule gModule_User32;
 extern glModule gModule_Kernel32;
 extern glModule gModule_GDI32;
--- WorkSpace/GameLibSource/glGlobalData.cpp
+++ WorkSpace/GameLibSource/glGlobalData.cpp
@@ -1,14 +1,6 @@
 
 #include "../GameLibInclude/glGlobalData.h"
 
-glWindow gWindow;
-glVideoDevice gVideoDevice;
-glSoundDevice gSoundDevice;
-glBatchSprite gBatchSprite;
-glBatchPrimitive gBatchPrimitive;
-glModule gModule_D3D9;
-glModule gModule_D3DX9;
-glModule gModule_DSound;
 glModule gModule_User32;
 glModule gModule_Kernel32;
 glModule gModule_GDI32;
